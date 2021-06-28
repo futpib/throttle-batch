bef7acf010c309c10b1be97a226e24bc05a28111
futpib <futpib@gmail.com>
2021-06-28 15:05:16 +0300

    init

---
+++ readme.md
@@ -0,0 +1,17 @@
+# throttle-batch
+
+> Throttle, but do not discard calls, instead batch them and run all at once
+
+[![Build Status](https://travis-ci.org/futpib/throttle-batch.svg?branch=master)](https://travis-ci.org/futpib/throttle-batch) [![Coverage Status](https://coveralls.io/repos/github/futpib/throttle-batch/badge.svg?branch=master)](https://coveralls.io/github/futpib/throttle-batch?branch=master)
+
+## Example
+
+```js
+// TODO
+```
+
+## Install
+
+```
+yarn add throttle-batch
+```
---
+++ src/index.test.ts
@@ -0,0 +1,101 @@
+import type { TestInterface } from 'ava'; // eslint-disable-line ava/use-test
+import anyTest from 'ava';
+import sinon from 'sinon';
+
+import throttleBatch from '.';
+
+const test = anyTest as TestInterface<{
+	clock: sinon.SinonFakeTimers;
+}>;
+
+test.beforeEach(t => {
+	const clock = sinon.useFakeTimers();
+
+	Object.assign(t.context, {
+		clock,
+	});
+});
+
+test.afterEach(t => {
+	t.context.clock.restore();
+});
+
+test.serial('one batch', async t => {
+	const { clock } = t.context;
+
+	const spy = sinon.spy();
+
+	const f = throttleBatch<[number]>((xs: number[]) => {
+		spy(xs);
+	});
+
+	f(1);
+	t.true(spy.calledOnceWith([ 1 ]));
+	f(2);
+	f(3);
+	f(4);
+	t.true(spy.calledOnce);
+
+	clock.tick(1);
+
+	t.true(spy.calledTwice);
+	t.deepEqual(spy.secondCall.firstArg, [ 2, 3, 4 ]);
+});
+
+test.serial('two batches', async t => {
+	const { clock } = t.context;
+
+	const spy = sinon.spy();
+
+	const f = throttleBatch<[number]>(spy, 10);
+
+	f(1);
+
+	t.true(spy.calledOnce);
+
+	f(2);
+	f(3);
+
+	t.true(spy.calledOnce);
+
+	clock.tick(9);
+
+	t.true(spy.calledOnce);
+
+	clock.tick(1);
+
+	t.true(spy.calledTwice);
+	t.deepEqual(spy.secondCall.firstArg, [ 2, 3 ]);
+
+	f(4);
+	f(5);
+
+	clock.tick(9);
+
+	t.true(spy.calledTwice);
+
+	clock.tick(1);
+
+	t.true(spy.calledThrice);
+});
+
+test.serial('regular calls', async t => {
+	const { clock } = t.context;
+
+	const spy = sinon.spy();
+
+	const f = throttleBatch<[number]>(spy, 2);
+
+	for (const i of Array.from({ length: 8 }).fill(undefined).map((_, i) => i)) {
+		f(i);
+		clock.tick(1);
+	}
+
+	clock.tick(1);
+
+	t.is(spy.callCount, 4);
+	t.deepEqual(spy.firstCall.firstArg, [ 0 ]);
+	t.deepEqual(spy.secondCall.firstArg, [ 1, 2, 3 ]);
+	t.deepEqual(spy.thirdCall.firstArg, [ 4, 5, 6 ]);
+	t.deepEqual(spy.lastCall.firstArg, [ 7 ]);
+});
